8ceb9f9b8077b2d8791ec0288447636f6dd4fb65
WBhlietue <WBhlietue@gmail.com>
2021-06-08 19:25:43 +0800

    tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import React from 'react'
 import Page1 from './scene/Page1';
 import Page2 from './scene/Page2';
@@ -7,99 +7,29 @@ import Page3 from "./scene/Page3"
 import Page4 from './scene/Page4';
 import Page5 from './scene/Page5';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Page1"
-          options={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: "#f4511e",
-              height: 50,
-            },
-
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white'
-            },
-
-          }}
-        >
+      <Tab.Navigator>
+        <Tab.Screen name="Page1">
           {props => <Page1 {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Page2"
-          options={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: "#f4511e",
-              height: 50,
-            },
-
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white'
-            },
-
-          }}
-        >
+        </Tab.Screen>
+        <Tab.Screen name="Page2">
           {props => <Page2 {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Page3"
-          options={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: "#f4511e",
-              height: 50,
-            },
-
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white'
-            },
-
-          }}
-        >
+        </Tab.Screen>
+        <Tab.Screen name="Page3">
           {props => <Page3 {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Page4"
-          options={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: "#f4511e",
-              height: 50,
-            },
-
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white'
-            },
-
-          }}
-        >
+        </Tab.Screen>
+        <Tab.Screen name="Page4">
           {props => <Page4 {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Page5"
-          options={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: "#f4511e",
-              height: 50,
-            },
-
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white'
-            },
-
-          }}
-        >
+        </Tab.Screen>
+        <Tab.Screen name="Page5">
           {props => <Page5 {...props} />}
-        </Stack.Screen>
-      </Stack.Navigator>
+        </Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
--- src/scene/Page1.tsx
+++ src/scene/Page1.tsx
@@ -1,14 +1,12 @@
 import { Text, View } from "react-native"
 import React from 'react'
-import SwitchBtn from "../components/SwitchBtns"
 
 const Page1 = (props:any) => {
     return (
         <View>
-            <Text style={{fontSize:40, textAlign:'center', top:0}}>
+            <Text style={{fontSize:40, textAlign:'center', top:200}}>
                 Page1
             </Text>
-            <SwitchBtn {...props} />
         </View>
     )
 }
